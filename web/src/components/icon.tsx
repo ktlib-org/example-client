d2b90478fe8eccb438507d00df9ad911208ba650
Aaron Freeman <aaron@freeman.zone>
2023-02-05 00:45:23 -0600

    fixing plus

--- web/src/components/icon.tsx
+++ web/src/components/icon.tsx
@@ -198,7 +198,7 @@ const IconSet = {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-full h-full"
+      className="w-6 h-6"
     >
       <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
