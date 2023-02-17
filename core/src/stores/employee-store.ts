bcaad2a8828841e0686ceeb7a40462e45a64c0f5
Aaron Freeman <aaron@freeman.zone>
2023-02-16 23:15:05 -0600

    making employee store observable

--- core/src/stores/employee-store.ts
+++ core/src/stores/employee-store.ts
@@ -1,12 +1,17 @@
 import { User } from "core/models/employee";
 import { Organization } from "core/models/organization";
-import { observable, runInAction } from "mobx";
+import { makeObservable, observable, runInAction } from "mobx";
 import { Store } from "./store";
 import { EmployeeApi } from "core/api";
 
 export default class EmployeeStore extends Store {
   @observable organizations: Organization[] = [];
   @observable users: User[] = [];
+
+  constructor() {
+    super();
+    makeObservable(this);
+  }
 
   async loadOrganizations() {
     const result = await EmployeeApi.organizations();
